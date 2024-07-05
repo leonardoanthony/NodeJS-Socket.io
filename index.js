2949a72191dba5a18ffca8b8abd8a744e619eced
leonardoanthony <leonardoanthony.dev@gmail.com>
2024-07-04 21:52:43 -0300

    :sparkles: feat: Adicionando notificação de quem entrou, saiu e enviou mensagem

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const usersOnline = {}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  // console.log('user connected');
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    io.emit('chat message', `${usersOnline[socket.id]} saiu!`);
+  });
+  socket.on('login_name', (msg) => {
+    usersOnline[socket.id] = msg;
+    io.emit('chat message', `${msg} entrou!`);
+    
   });
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', `${usersOnline[socket.id]}: ${msg}`);
   });
+
+
 });
 
 server.listen(3000, () => {
